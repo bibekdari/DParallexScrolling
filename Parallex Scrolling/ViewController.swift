24e501f5f31808359597be2bb248ea7bf8d4c6db
bibek <esbibek07@gmail.com>
2016-08-25 12:29:53 +0545

    scrolling upwards as exprected

--- Parallex Scrolling/ViewController.swift
+++ Parallex Scrolling/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var parentScrollView: UIScrollView!
+    @IBOutlet weak var childScrollView: UITableView!
+    
+    var goingUp: Bool?
+    
+    let fixedHeight: CGFloat = 25
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        parentScrollView.delegate = self
+        childScrollView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +32,28 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "CELL NO \(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        goingUp = scrollView.panGestureRecognizer.translationInView(scrollView).y < 0
+        if goingUp! {
+            if scrollView == childScrollView {
+                if parentScrollView.contentOffset.y + parentScrollView.frame.height < parentScrollView.contentSize.height {
+                    parentScrollView.contentOffset.y = min(parentScrollView.contentOffset.y + childScrollView.contentOffset.y, parentScrollView.contentSize.height - parentScrollView.frame.height)
+                    childScrollView.contentOffset.y = 0
+                }
+            }
+        }
+    }
+}
